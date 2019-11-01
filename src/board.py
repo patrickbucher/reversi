127f1edfb27efa26dc2180fd2b6b55276f098cb9
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-11-01 10:18:42 +0100

    initialize board with starting position

--- src/board.py
+++ src/board.py
@@ -5,12 +5,20 @@ class Board:
     DIM = 8
 
     FIELD_EMPTY = 0
-    FIELD_P1 = 1
-    FIELD_P2 = 2
+    FIELD_BLACK = 1
+    FIELD_WHITE = 2
 
 
     def __init__(self):
-        self.__fields  = np.zeros([self.DIM, self.DIM], dtype=np.uint8)
+        self.__fields = np.zeros([self.DIM, self.DIM], dtype=np.uint8)
+
+        whites = [[self.DIM/2-1, self.DIM/2-1], [self.DIM/2, self.DIM/2]]
+        blacks = [[self.DIM/2, self.DIM/2-1], [self.DIM/2-1, self.DIM/2]]
+
+        for [r, c] in whites:
+            self.__fields[(int(r), int(c))] = self.FIELD_WHITE
+        for [r, c] in blacks:
+            self.__fields[(int(r), int(c))] = self.FIELD_BLACK
 
 
     @property
--- src/board_test.py
+++ src/board_test.py
@@ -6,9 +6,23 @@ from board import Board
 
 class BoardTest(unittest.TestCase):
 
+
     def test_init_empty(self):
         board = Board()
-        print(board.fields)
+
+        white = [[3, 3], [4, 4]]
+        black = [[3, 4], [4, 3]]
+        empty = [[r, c] for r in range(0, 8) for c in range(0, 8) if r not in [3, 4] or c not in [3, 4]] 
+
+        for [r, c] in white:
+            self.assertEqual(board.fields[r, c], Board.FIELD_WHITE)
+
+        for [r, c] in black:
+            self.assertEqual(board.fields[r, c], Board.FIELD_BLACK)
+
+        for [r, c] in empty:
+            self.assertEqual(board.fields[r, c], Board.FIELD_EMPTY)
+
 
 if __name__ == '__main__':
     unittest.main()
