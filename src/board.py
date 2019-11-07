0b4a9faae0d71e670637d60cae287cf2a07e3b89
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-11-07 16:09:57 +0100

    formatted code

--- src/board.py
+++ src/board.py
@@ -1,26 +1,37 @@
 import numpy as np
 
+
 class Board:
 
     DIM = 8
-
     FIELD_EMPTY = 0
     FIELD_BLACK = 1
     FIELD_WHITE = 2
 
+    def __init__(self, fields):
+        if fields.shape != (self.DIM, self.DIM):
+            raise ValueError('unsupported dimensions', fields.shape)
 
-    def __init__(self):
-        self.__fields = np.zeros([self.DIM, self.DIM], dtype=np.uint8)
+        self._fields = fields
 
-        whites = [[self.DIM/2-1, self.DIM/2-1], [self.DIM/2, self.DIM/2]]
-        blacks = [[self.DIM/2, self.DIM/2-1], [self.DIM/2-1, self.DIM/2]]
+    @classmethod
+    def start_position(cls):
+        fields = np.zeros([cls.DIM, cls.DIM], dtype=np.uint8)
+
+        whites = [[cls.DIM/2-1, cls.DIM/2-1], [cls.DIM/2, cls.DIM/2]]
+        blacks = [[cls.DIM/2, cls.DIM/2-1], [cls.DIM/2-1, cls.DIM/2]]
 
         for [r, c] in whites:
-            self.__fields[(int(r), int(c))] = self.FIELD_WHITE
+            fields[(int(r), int(c))] = cls.FIELD_WHITE
         for [r, c] in blacks:
-            self.__fields[(int(r), int(c))] = self.FIELD_BLACK
+            fields[(int(r), int(c))] = cls.FIELD_BLACK
 
+        return Board(fields)
+
+    @classmethod
+    def from_fields(cls, fields):
+        return Board(fields)
 
     @property
     def fields(self):
-        return self.__fields
+        return self._fields
--- src/board_test.py
+++ src/board_test.py
@@ -2,17 +2,23 @@
 
 import unittest
 
+import numpy as np
+
 from board import Board
+
 
 class BoardTest(unittest.TestCase):
 
-
     def test_init_empty(self):
-        board = Board()
+        board = Board.start_position()
 
         white = [[3, 3], [4, 4]]
         black = [[3, 4], [4, 3]]
-        empty = [[r, c] for r in range(0, 8) for c in range(0, 8) if r not in [3, 4] or c not in [3, 4]] 
+        empty = [
+            [r, c]
+            for r in range(0, 8)
+            for c in range(0, 8) if r not in [3, 4] or c not in [3, 4]
+        ]
 
         for [r, c] in white:
             self.assertEqual(board.fields[r, c], Board.FIELD_WHITE)
@@ -22,6 +28,14 @@ class BoardTest(unittest.TestCase):
 
         for [r, c] in empty:
             self.assertEqual(board.fields[r, c], Board.FIELD_EMPTY)
+
+    def test_init_with_field(self):
+        fields = np.zeros((8, 8), dtype=np.uint8)
+        board = Board.from_fields(fields)
+
+        for r in range(0, 8):
+            for c in range(0, 8):
+                self.assertEqual(board.fields[r, c], Board.FIELD_EMPTY)
 
 
 if __name__ == '__main__':
